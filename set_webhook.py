f7bd9aec3304c2dab1c1f55e762b306d67f05b6c
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-02 19:52:20 +0500

    setwebhook;

---
+++ set_webhook.py
@@ -0,0 +1,11 @@
+from telegram import Bot
+import os
+
+TOKEN = os.environ.get('TOKEN')
+
+bot = Bot(TOKEN)
+
+# url = 'https://python2022p.pythonanywhere.com/bot'
+
+# print(bot.set_webhook(url))
+print(bot.get_webhook_info())
