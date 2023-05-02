8c4fedcbb61d8cdb240030dac2b41ff1861286e8
djumanov <diyorbekjumanov1120@gmail.com>
2023-05-02 19:52:58 +0500

    setwebhook;

--- set_webhook.py
+++ set_webhook.py
@@ -5,7 +5,7 @@ TOKEN = os.environ.get('TOKEN')
 
 bot = Bot(TOKEN)
 
-# url = 'https://python2022p.pythonanywhere.com/bot'
+url = 'https://python2022p.pythonanywhere.com/bot'
 
-# print(bot.set_webhook(url))
+print(bot.set_webhook(url))
 print(bot.get_webhook_info())
